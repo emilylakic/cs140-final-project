5c07d22b4a188c43729185741449269ba44c1efd
Jordana Simon <jsimon7@binghamton.edu>
2017-12-05 11:50:23 -0500

    Update ControlPanel.java

--- project/ControlPanel.java
+++ project/ControlPanel.java
@@ -19,32 +19,38 @@ public class ControlPanel implements Observer{
 	private JButton reloadButton = new JButton("Reload");
 	
 	public ControlPanel(ViewMediator mediator) {
-        this.mediator = mediator;
-        mediator.addObserver(this);
+        	this.mediator = mediator;
+        	mediator.addObserver(this);
     }
 	
 	public JComponent createControlDisplay() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(1, 0));
+        	JPanel panel = new JPanel();
+        	panel.setLayout(new GridLayout(1, 0));
         
-        stepButton.setBackground(Color.WHITE);
-        stepButton.addActionListener(e -> mediator.step());
-        panel.add(stepButton);
+        	stepButton.setBackground(Color.WHITE);
+        	stepButton.addActionListener(e -> mediator.step());
+        	panel.add(stepButton);
         
-        clearButton.setBackground(Color.WHITE);
-        clearButton.addActionListener(e -> mediator.clearJob());
-        panel.add(clearButton);
+        	clearButton.setBackground(Color.WHITE);
+        	clearButton.addActionListener(e -> mediator.clearJob());
+        	panel.add(clearButton);
 
-        runButton.setBackground(Color.WHITE);
-        runButton.addActionListener(e -> mediator.toggleAutoStep());
-        panel.add(runButton);
+        	runButton.setBackground(Color.WHITE);
+        	runButton.addActionListener(e -> mediator.toggleAutoStep());
+        	panel.add(runButton);
         
-        JSlider slider = new JSlider(5,1000);
-        slider.addChangeListener(e -> mediator.setPeriod(slider.getValue())); // have not written the methods yet
-        panel.add(slider);
-        return panel;
+        	JSlider slider = new JSlider(5,1000);
+        	slider.addChangeListener(e -> mediator.setPeriod(slider.getValue())); // have not written the methods yet
+        	panel.add(slider);
+        	
+		return panel;
+	}
         
         @Override
-        public void update(Observable arg0, Object arg1) {		
+        public void update(Observable arg0, Object arg1) {
+		runButton.setEnabled(mediator.getCurrentState().getRunPauseActive());
+        	stepButton.setEnabled(mediator.getCurrentState().getStepActive());
+        	clearButton.setEnabled(mediator.getCurrentState().getClearActive());
+       		reloadButton.setEnabled(mediator.getCurrentState().getReloadActive());
         }
 }
